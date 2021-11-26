4fe894ddddef9f4ea4a2190f56a937b012c490ad
Alice Schneider <alice.schneider@live.fr>
2021-11-26 14:25:45 +0100

    Implémentation  helmet + ajout ' images' dans  gitignore

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express'); // on importe express
 
 const bodyParser = require('body-parser'); //on importe bodyparser pour extraire l'objet JSON des requêtes
 const mongoose = require('mongoose'); // on importe mogoose
+const helmet = require('helmet'); //on importe helmet pour renforcer la sécurité (configuration d'en-tetes HTTP moins vulnérables)
 const path = require('path'); //on importe path pour avoir accès au chemin de notre système de fichiers
 
 const sauceRoutes = require('./routes/sauce'); //on importe nos routes sauce
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+app.use(helmet());
 app.use('/images', express.static(path.join(__dirname, 'images'))); // on demande à notre fichier app.js de servir notre dossier statique 'images' quand il y a une requête
 
 app.use('/api/sauces', sauceRoutes);
